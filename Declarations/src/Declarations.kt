71a192da352fd83041264ed6a9e4cc0cf7aac202
Soh <dexsjw@gmail.com>
2025-07-16 18:09:44 +0800

    updated with lesson's code

--- Declarations/src/Declarations.kt
+++ Declarations/src/Declarations.kt
@@ -1,14 +1,53 @@
 typealias EmployeeSet = Set<Employee>
 
 fun main(args: Array<String>) {
+
+//    val employeeOne = Employee("Mary", 1)
+//    val employeeTwo = Employee("John", 2)
+//    val employeeThree = Employee("John", 2)
+//    println(employeeOne === employeeTwo)
+//    println(employeeTwo === employeeThree)
+//    println(employeeOne == employeeTwo)
+//    println(employeeTwo == employeeThree)
+//    println(employeeOne.equals(employeeTwo))
+//    println(employeeTwo.equals(employeeThree))
+//    val employeeFour = employeeTwo
+//    println(employeeFour === employeeTwo)
+//
+//    println(employeeFour != employeeTwo)
+//    println(employeeFour !== employeeTwo)
+//    println(employeeTwo != employeeThree)
+//    println(employeeTwo !== employeeThree)
+//
+//    val something: Any = employeeFour
+//    if (something is Employee) {
+////        val newEmployee = something as Employee
+//        println(something.name)
+//    }
+
     var number: Int
     number = 10
     number = 20
 
-    val employees: EmployeeSet
+//    val names = arrayListOf("John", "Jane", "Mary")
+//    println(names[1])
+//
+//    val employees: EmployeeSet
 
     val employee1 = Employee("Lynn Jones", 500)
     employee1.name = "Lynn Smith"
+
+    println(employee1)
+
+    val change = 4.22
+    println("To show the value of change, we use \$change")
+    println("Your change is $$change")
+
+    val numerator = 10.99
+    val denominator = 20.00
+    println("The value of $numerator divided by $denominator is ${numerator/denominator}")
+
+    println("The employee's id is ${employee1.id}")
 
     val employee2: Employee
     val number2 = 100
@@ -21,5 +60,17 @@ fun main(args: Array<String>) {
 }
 
 class Employee(var name: String, val id: Int) {
+
+    override fun equals(obj: Any?): Boolean {
+        if (obj is Employee) {
+            return name == obj.name && id == obj.id
+        }
+
+        return false
+    }
+
+    override fun toString(): String {
+        return "Employee(name='$name', id=$id)"
+    }
 
 }
