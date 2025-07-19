5cd26add55c9858a4f93e1be23bfe7885ccb1375
dextersjw <dextersjw@gmail.com>
2025-07-20 00:28:11 +0800

    updated files for Section4-30 lesson

--- Section4/DataTypes/src/academy/learnprogramming/javacode/DummyClass.java
+++ Section4/DataTypes/src/academy/learnprogramming/javacode/DummyClass.java
@@ -2,8 +2,14 @@ package academy.learnprogramming.javacode;
 
 public class DummyClass {
 
-    public String isVacationTime(Boolean onVacation) {
+    public String isVacationTime(boolean onVacation) {
         return onVacation ? "I'm on vacation!" : "I'm working";
+    }
+
+    public void printNumbers(int[] numbers) {
+        for (int number : numbers) {
+            System.out.println(number);
+        }
     }
 
 }
