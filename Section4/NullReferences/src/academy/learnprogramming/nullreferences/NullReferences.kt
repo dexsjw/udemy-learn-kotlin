8806caf21299ace6426f94ebc45fc40fc4904fdb
dextersjw <dextersjw@gmail.com>
2025-07-20 22:51:05 +0800

    completed Section4-32

--- Section4/NullReferences/src/academy/learnprogramming/nullreferences/NullReferences.kt
+++ Section4/NullReferences/src/academy/learnprogramming/nullreferences/NullReferences.kt
@@ -2,33 +2,57 @@ package academy.learnprogramming.nullreferences
 
 fun main(args: Array<String>) {
 
-    val str: String? = null
-    println("What happens when we do this: ${str?.uppercase()}")
-    // str?.uppercase() is similar to this in Java:
-    /*
-    if (str == null) {
-        null
-    } else {
-        str.uppercase()
-    }
-    */
+    val str: String? = "This isn't null"
+    str?.let { printText(it) } // ".let" wants a lambda expression
 
-    val str2 = str ?: "This is the default value"
-    println(str2)
+    val str4: String? = null
+    val anotherStr = "This isn't nullable"
+    println(str4 == anotherStr)
 
-    // Elvis operator - "?:"
-    // val str2 = str ?: "This is the default value"
-    // is shorthand for:
-    /*
-    if (str == null) {
-        str2 = "This is the default value"
-    } else {
-        str2 = str
-    }
-    */
+    val str2 = str!!
+    val str3 = str2.uppercase()
 
-    val something: Any = arrayOf(1, 2, 3, 4)
-    val str3 = something as? String
-    println(str3)
-    println(str3?.uppercase())
+//    val str4 = str!!.uppercase()
+//    // str?.uppercase() is similar to this in Java:
+//    /*
+//    if (str == null) {
+//        throw exception
+//    } else {
+//        str.uppercase()
+//    }
+//    */
+//
+//    println("What happens when we do this: ${str?.uppercase()}")
+//    // str?.uppercase() is similar to this in Java:
+//    /*
+//    if (str == null) {
+//        null
+//    } else {
+//        str.uppercase()
+//    }
+//    */
+//
+//    val str2 = str ?: "This is the default value"
+//    println(str2)
+//
+//    // Elvis operator - "?:"
+//    // val str2 = str ?: "This is the default value"
+//    // is shorthand for:
+//    /*
+//    if (str == null) {
+//        str2 = "This is the default value"
+//    } else {
+//        str2 = str
+//    }
+//    */
+//
+//    val something: Any = arrayOf(1, 2, 3, 4)
+//    val str3 = something as? String
+//    println(str3)
+//    println(str3?.uppercase())
+
+}
+
+fun printText(text: String) {
+    println(text)
 }
