6f2a4654e9f343e200f0d4b2bdf0319768041b9a
dextersjw <dextersjw@gmail.com>
2025-07-28 15:44:56 +0800

    rearranged notes

--- Section5/AccessModifiers/src/academy/learnprogramming/accessmodifiers/AccessModifiers.kt
+++ Section5/AccessModifiers/src/academy/learnprogramming/accessmodifiers/AccessModifiers.kt
@@ -1,16 +1,5 @@
 package academy.learnprogramming.accessmodifiers
 
-fun main(args: Array<String>) {
-
-    val emp = Employee()
-    println(emp)
-
-}
-
-private class Employee {
-
-}
-
 /*                               Top Level Items                                   */
 // Access Modifier      Kotlin                              Java
 // private              Visible within the same file        Can't be used
@@ -21,3 +10,14 @@ private class Employee {
 // When compiled, Kotlin's access modifiers will be compiled to access modifiers that JVM understands
 // Kotlin's private is compiled to "package-private" ("default" in Java)
 // Kotlin's internal is compiled to "public" (Java)
+
+fun main(args: Array<String>) {
+
+    val emp = Employee()
+    println(emp)
+
+}
+
+private class Employee {
+
+}
