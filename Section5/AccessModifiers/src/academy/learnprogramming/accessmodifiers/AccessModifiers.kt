d80603202d3a7e816edc5137fcf662633d41689a
dextersjw <dextersjw@gmail.com>
2025-07-28 15:42:55 +0800

    updated notes

--- Section5/AccessModifiers/src/academy/learnprogramming/accessmodifiers/AccessModifiers.kt
+++ Section5/AccessModifiers/src/academy/learnprogramming/accessmodifiers/AccessModifiers.kt
@@ -19,4 +19,5 @@ private class Employee {
 // internal             Visible within the same module      N/A
 
 // When compiled, Kotlin's access modifiers will be compiled to access modifiers that JVM understands
-// Kotlin's
+// Kotlin's private is compiled to "package-private" ("default" in Java)
+// Kotlin's internal is compiled to "public" (Java)
