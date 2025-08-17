560c8679df6de78dfda5d94f4f6a949359157f3c
dextersjw <dextersjw@gmail.com>
2025-08-18 02:13:07 +0800

    completed Section8-79

---
+++ Section8/WalkFileTree/src/academy/learnprogramming/walkfiletree/WalkFileTree.kt
@@ -0,0 +1,11 @@
+package academy.learnprogramming.walkfiletree
+
+import java.io.File
+
+fun main(args: Array<String>) {
+
+    File(".").walkTopDown()
+        .filter { it.name.endsWith(".kt")}
+        .forEach { println(it) }
+
+}
